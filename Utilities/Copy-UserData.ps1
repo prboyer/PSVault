c63c0e7859951f950c8d083685667adb36991775
Paul Boyer <pboyer2@wisc.edu>
2021-02-02 13:41:47 -0600

    added file to repo. Needs to be cleaned up and commented. Removed old ROBOCOPY script.

---
+++ Utilities/Copy-UserData.ps1
@@ -0,0 +1,17 @@
+﻿#************************
+#Profile Move Script
+#Paul Boyer , 11-17-2020
+#************************
+#######################################################################
+[String]$oldProfilePath = "E:\Users\kszabados"
+
+[String]$newProfilePath = "C:\Users\knspear"
+#######################################################################
+
+#directories that need to be copied
+[String]$directories = @('Contacts','Desktop','Documents','Downloads','Favorites','Music','Pictures','Videos')
+
+#extraneous directories
+[String]$extraneousDirectories = @('AppData','.oracle_jre_usage','Links','Saved Games','Searches','Local Settings','Application Data','B')
+
+Start-Process -FilePath robocopy.exe -ArgumentList "$oldProfilePath $newProfilePath /S /ETA /R:1 /W:3 /XX /XD $extraneousDirectories" -Wait -NoNewWindow
