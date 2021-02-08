540d2632dc70a5932be56dcf4d30d2428fadca00
Paul Boyer <pboyer2@wisc.edu>
2021-02-08 08:16:26 -0600

    Still trying to get the module file to import properly.

--- Export-MDReadMe.ps1
+++ Export-MDReadMe.ps1
@@ -6,7 +6,7 @@ function Export-MDReadMe {
     Import-Module -Name platyPS
 
     # Create the PS1M Module file from a collection of PS1 files
-    $Path = ".\Utilities"
+    $Path = "$PSScriptRoot\Utilities"
 
     $files = Get-ChildItem -Path $Path -Filter "*.ps1"
     
@@ -22,11 +22,9 @@ function Export-MDReadMe {
 
     # Import the module file that was just created
     Import-Module -FullyQualifiedName $(Get-ChildItem -Path $Path -Filter "*.psm1").FullName -Scope Local -Force
-
+   
     # Call PlatyPS
     New-MarkdownHelp -Module $(Get-ChildItem -Path $Path -Filter "*.psm1").Name.Replace(".psm1","") -AlphabeticParamsOrder -OutputFolder $Path
-
-
 }
 
 
