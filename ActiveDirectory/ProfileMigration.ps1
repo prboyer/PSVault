9969e93c97e9982d1639a69ddfac324896a4af24
Paul Boyer <pboyer2@wisc.edu>
2021-02-18 09:35:06 -0600

    WIP: re-writing the profile migration script.

--- ActiveDirectory/ProfileMigration.ps1
+++ ActiveDirectory/ProfileMigration.ps1
@@ -50,53 +50,50 @@ foreach($profile in $profileList){
     }
 }
 
+
 function Migrate-UserProfile {
-    [CmdletBinding()]
-    param (
-        [Parameter(ParameterSetName='Single',Mandatory=$true)]
-        [String]
-        $Username,
-        [Parameter(ParameterSetName='Multiple',Mandatory=$true)]
-        [String[]]
-        $Usernames,
-        [Parameter(ParameterSetName='Single',Mandatory=$true)]
-        [Parameter(ParameterSetName='Multiple',Mandatory=$true)]
-        [String]
-        $ProfileServer,
-        [Parameter(ParameterSetName='Single',Mandatory=$false)]
-        [Parameter(ParameterSetName='Multiple',Mandatory=$false)]
-        [switch]
-        $NoCheckVersion,
-        [Parameter(ParameterSetName='Single',Mandatory=$false)]
-        [Parameter(ParameterSetName='Multiple',Mandatory=$false)]
-        [switch]
-        $Force
+  param (
+    [Parameter(Mandatory=$true)]
+    [String[]]
+    $Usernames,
+    [Parameter(Mandatory=$True)]
+    [ValidateScript({Test-Path -Path $_ -PathType 'Container'})]
+    [System.IO.Path]
+    $ProfileServer
+  )
+ 
+  #directories that need to be copied
+  [String[]]$directories = @('Contacts','Desktop','Documents','Downloads','Favorites','Music','Pictures','Videos')
 
-    )
-    # VARIABLES
-      #directories that should not be copied
-      [String[]]$excludedDirectories = ('AppData','.oracle_jre_usage','Links','Saved Games','Searches','Local Settings','Application Data','OneDrive*','*.*')  
+  #extraneous directories
+  #$extraneousDirectories = 'AppData','.oracle_jre_usage','Links','Saved Games','Searches'
 
-    ##
 
-    # Validate that the path to the profile server can be resolved
-    try {
-      if(-not (Test-Path $ProfileServer)){
-        Throw [System.IO.DirectoryNotFoundException]::new()
+  Write-Host "Profile Migration Script" -ForegroundColor Cyan
+
+  #copy contents each directory in $directories to the appropriate location in the user's .V6 profile
+  #traverse each profile in the profile list array
+  foreach($profile in $Usernames){
+
+    #navigate to V2 directory for each user in the profile list array
+    if($profileServer.contains($profile.V2)){
+      Write-Host "Copying $profile.V2 --> $profile.V6 on $profileServer" -ForegroundColor Yellow
+      Set-Location $profileServer\$profile.V2
+      Get-Location | Get-ChildItem 
+
+      #copy each directory in the directories array from the user's V2 profile to their V6 profile
+      foreach($dir in $directories){
+        Write-Host "Copying $dir" -ForegroundColor Yellow
+        Copy-Item $dir -Destination $profileServer\$profile.V6 -Recurse -Force -Verbose
+        Write-Host "Complete" -ForegroundColor Green
       }
     }
-    catch [System.IO.DirectoryNotFoundException]{
-      Write-Error -Category ObjectNotFound -Message $("Unable to resolve path to profile server at: {0}" -f $ProfileServer)
+    else{
+
+        #error handling for if a V2 directory does not exist on the server
+        Write-Host "No .V2 directory exists for $profile" -ForegroundColor Red
     }
-
-    
-
-
-
-
-
-
-
-
+  }
+  Write-Host "Complete" -ForegroundColor Green -BackgroundColor Black
 
 }
