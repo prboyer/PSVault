29d9708270addb6497516dbfdbf2040ef53ca6dd
Paul Boyer <pboyer2@wisc.edu>
2021-03-11 10:09:42 -0600

    Comments applied

--- GroupPolicy/Check-GPPermissions.ps1
+++ GroupPolicy/Check-GPPermissions.ps1
@@ -1,3 +1,30 @@
+<#
+.SYNOPSIS
+Script to report GPOs in a domain that do not have accessible ACLs applied.
+
+.DESCRIPTION
+The script will evaluate which GPOs are lacking 'Apply' permissions for either Authenticated Users or Domain Computers. If neither permission is applied, the group
+policies themselves will not be applied. However, sometimes when a GPO is to be limited to a specific security group it is necessary to make the ACLs more targeted. With this in mind, 
+certain GUIDs can excluded from the report.
+
+.PARAMETER FilePath
+Path to where the report should be saved. This should be a .txt file. 
+
+.PARAMETER Fix
+Switch parameter that will send the script into an interactive mode to fix the missing ACLs on GPOs
+
+.PARAMETER Exclude
+String Array of GUIDs to exclude from evaluation
+
+.EXAMPLE
+Check-GPPermissions -FilePath C:\Temp\Report.txt
+
+.EXAMPLE
+Check-GPPermissions -Fix
+
+.NOTES
+
+#>
 function Check-GPPermissions{
     [CmdletBinding()]
     param (
