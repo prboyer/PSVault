a3ae1c3759e7a07bfbcbebd6eedaea2a5bae54da
Paul Boyer <pboyer2@wisc.edu>
2021-03-11 10:05:13 -0600

    Added Check-GPPermissions script. Still needs comments.

---
+++ GroupPolicy/Check-GPPermissions.ps1
@@ -0,0 +1,73 @@
+function Check-GPPermissions{
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]
+        $FilePath,
+        [Parameter()]
+        [Switch]
+        $Fix,
+        [Parameter()]
+        [String[]]
+        $Exclude
+    )
+    #Requires -Module GroupPolicy
+    Import-Module GroupPolicy
+
+    #List of GPOs that don't have proper permissions
+    [System.Collections.ArrayList]$BadPermissions = [System.Collections.ArrayList]::new()
+
+    #Get all GPOs in current domain
+    [Object[]]$DomainGPOs = Get-GPO -All
+    
+    # For each GPO in $DomainGPOs, this block of logic checks to see if the GPO has an Apply ACL for either Authenticated Users or Domain Computers
+    foreach($gpo in $DomainGPOs){
+        if ((($gpo.GetSecurityInfo() | ?{$_.Trustee.Name -eq "Authenticated Users"} | Select-Object Permission).Permission -ine "GpoApply") -and 
+        (($gpo.GetSecurityInfo() | ?{$_.Trustee.Name -eq "Domain Computers"} | Select-Object Permission).Permission -ine "GpoApply")){
+            # Add the offending GPOs to the array list. Using Out-Null to suppress the index # returned to the console after adding to the list
+            $BadPermissions.Add($gpo.Id.Guid) | Out-Null
+        }
+    }
+
+    # Process the list of GUIDs to exclude from evaluation
+    foreach($e in $Exclude){
+        # need to covert the GUID to lowercase so that matching will work properly. GPO cmdlets report GUIDs in lowercase, and don't do case-insensitive matching
+        $BadPermissions.Remove($e.ToLower())
+    }
+
+    Write-Information "`nGPOs that are missing both `"Authenticated Users`" and `"Domain Computers`" application scopes" -InformationAction Continue
+
+    # Write out the polices that need to have new permissions applied. If -FilePath is supplied, pipe to the console and a file
+    foreach($guid in $BadPermissions){
+        if($FilePath -ne ""){
+            # print out the results to the console and to the output file
+            Get-GPO -Guid $guid | Select-Object DisplayName, Owner, ID, Description, GpoStatus, CreationTime, ModificationTime | Tee-Object -FilePath $FilePath -Append
+        }else{
+            $formattedResults += @(Get-GPO -Guid $guid | Select-Object DisplayName, Owner, ID, GpoStatus, CreationTime, ModificationTime)
+        }
+    }
+    # display the results to just the console, but in a cleaner fashion
+    $formattedResults | Sort-Object DisplayName | Format-Table -AutoSize
+
+    # If fix is supplied, add the missing delegations to the GPOs interactively
+    if($Fix){
+        foreach($guid in $BadPermissions){
+            Write-Host $(Get-GPO -Guid $guid).DisplayName -ForegroundColor Yellow
+            (Get-GPO -Guid $guid).GetSecurityInfo() | Format-Table
+            [int]$z = Read-Host -Prompt "Which ACL would you like to apply? `n[1] Authenticated Users `n[2] Domain Computers `n[3] Both`n[0] Exit`nPress `'Enter`' to skip without making changes"
+            
+            # add the missing ACL specified by the user
+            switch ($z) {
+                1 {Get-GPO -Guid $guid | Set-GPPermission -PermissionLevel GpoApply -TargetName "Authenticated Users" -TargetType Group}
+                2{Get-GPO -Guid $guid | Set-GPPermission -PermissionLevel GpoApply -TargetName "Domain Computers" -TargetType Group}
+                3{
+                    Get-GPO -Guid $guid | Set-GPPermission -PermissionLevel GpoApply -TargetName "Authenticated Users" -TargetType Group;
+                    Get-GPO -Guid $guid | Set-GPPermission -PermissionLevel GpoApply -TargetName "Domain Computers" -TargetType Group; 
+                }
+                0{exit;}
+                Default {break;}
+            }
+        }
+    }
+}
+Check-GPPermissions -Fix
