ddb2193f51d84e04b7d2d36f8915aff325d3b142
Paul Boyer <pboyer2@wisc.edu>
2021-04-14 10:50:52 -0500

    created script to get current TLS settings

---
+++ Windows10/Get-TLSVersion.ps1
@@ -0,0 +1,28 @@
+function Get-TLSVersion {
+    <#
+    .SYNOPSIS
+    Short script that returns the current TLS version settings
+    
+    .DESCRIPTION
+    Script uses .NET methods to get the current TLS version settings. Then prints out results to console. Also returns current setting as a string.
+    
+    .EXAMPLE
+    Get-TLSVersion
+    
+    .OUTPUTS
+    String with current system setting for connection security.
+
+    .LINK
+    https://www.eshlomo.us/check-and-update-powershell-tls-version/
+    
+    .NOTES
+        Author: Paul Boyer
+        Date: 4-14-21
+    #>
+
+    $output = [enum]::GetNames([Net.SecurityProtocolType]) | Select @{name='Name'; expression={$_}} , @{name='CurrentSetting'; expression={if($_ -eq [Net.ServicePointManager]::SecurityProtocol){" < "}}}
+
+    Write-Information $($output | Out-String) -InformationAction Continue
+
+    return [Net.ServicePointManager]::SecurityProtocol.ToString()
+}
