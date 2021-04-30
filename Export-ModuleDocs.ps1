c4861083107179d47955c19677125ffc3d067043
Paul Boyer <pboyer2@wisc.edu>
2021-04-30 15:34:56 -0500

    cleaned up comments and removed -Concat function. May re-add this later when I recall what it was supposed to do (I didn't comment the first time).

--- Export-ModuleDocs.ps1
+++ Export-ModuleDocs.ps1
@@ -2,8 +2,6 @@ function Export-ModuleDocs {
     param (
         [String]
         $Path,
-        [Switch]
-        $Concat,
         [switch]
         $IncludeIO
     )
@@ -14,18 +12,11 @@ function Export-ModuleDocs {
     # Get all the PS1 files in the directory
     $files = Get-ChildItem -Path $Path -Filter "*.ps1"
 
-    # Append the contents of all the PS1 files into a PSM1 module file
-    # if ($Concat) {
-    #     $files | %{
-    #         Get-Content -Path $_.FullName | Out-File -FilePath "$Path\PSVault-$(Split-Path -Path $Path -Leaf).psm1" -Append -Force
-    #     }
-    # }else {
-        # Dot source all the PS1 files in a PSM1 module file
-        $files | ForEach-Object{
-            [string]$(". `"$Path\"+$_.Name+"`"") | Out-File -FilePath "$Path\PSVault-$(Split-Path -Path $Path -Leaf).psm1" -Append -Force 
-        }
-    # }
-
+    # Dot source all the PS1 files in a PSM1 module file
+    $files | ForEach-Object{
+        [string]$(". `"$Path\"+$_.Name+"`"") | Out-File -FilePath "$Path\PSVault-$(Split-Path -Path $Path -Leaf).psm1" -Append -Force 
+    }
+    
     # assign a new guid
     $moduleGUID = New-Guid
 
@@ -80,12 +71,10 @@ function Export-ModuleDocs {
         })
     }
 
-   
-
     # generate the module readme file
     Update-MarkdownHelpModule -ModulePagePath "$Path\README.md" -Path "$Path\Docs" -RefreshModulePage -Force| Out-Null
 
-    # change the pathing of the README.md file to be correct
+    # change the pathing of the README.md file to support putting individual script documentation in another directory
     Set-Content -Path "$Path\README.md" -Value $(get-content -path "$Path\README.md" | ForEach-Object{
         if($_ -match "###"){
             $_.ToString().Replace($_,$_.ToString().Insert($_.ToString().IndexOf('(')+1,"Docs/"))
