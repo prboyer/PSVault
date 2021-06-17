c8898ea778b71b0ba352dfa6404a9156dbf5cf1e
Paul Boyer <pboyer2@wisc.edu>
2021-06-17 16:28:22 -0500

    updated script to allow for running report from a list of computer names.

--- SCCM_MECM/Build-MECMComputerReport.ps1
+++ SCCM_MECM/Build-MECMComputerReport.ps1
@@ -8,6 +8,9 @@ function Build-MECMComputerReport{
     
     .PARAMETER Path
     The path where a csv report should be saved
+
+    .PARAMETER ComputerNames
+    A list of computer names to run the report against
     
     .PARAMETER CollectionName
     The name of the device collection in MECM to report on. Accepts wildcards. 
@@ -17,6 +20,9 @@ function Build-MECMComputerReport{
     
     .PARAMETER ProviderMachineName
     The MECM endpoint that the script should be run against, typically a DP or another MECM server.
+
+    .INPUTS
+    Either the name of an existing MECM device collection or a list of computer names.
 
     .OUTPUTS
     A CSV report with the date run appended. "MECM_Report-$(Get-Date -Format FileDate).csv"
@@ -33,10 +39,13 @@ function Build-MECMComputerReport{
         [ValidateNotNullOrEmpty()]
         [String]
         $Path,
-        [Parameter(Mandatory=$true)]
+        [Parameter(ParameterSetName="Collection")]
         [ValidateNotNullOrEmpty()]
         [String]
         $CollectionName,
+        [Parameter(ParameterSetName="Array")]
+        [String[]]
+        $ComputerNames,
         [Parameter(Mandatory=$true)]
         [ValidateNotNullOrEmpty()]
         [String]
@@ -78,33 +87,63 @@ function Build-MECMComputerReport{
     ########## End Site Configuration ###########
     Import-Module ActiveDirectory
 
-    # Try to get the CMCollection by name, error handle if not found
-    try {
-        $collection = (Get-CMCollection -Name $CollectionName -ForceWildcardHandling -CollectionType Device)
-    }
-    catch {
-        Write-Error -Message $("Unable to resolve CM Collection {0}" -f $CollectionName) -ErrorAction Stop -Category ObjectNotFound
-    }
+    # Declare an Object[] to store results
+    [Object[]]$script:ObjectArray = @();
 
-    [Object[]]$ObjectArray = @();
-
-    # Get the devices in the collection, then mine data to create report
-    Get-CMDevice -Collection $collection | %{
-        [PSCustomObject]$Object = [PSCustomObject]@{
-            Name = $_.name
-            Vendor = $(Get-ADComputer $_.name -Properties sSCCHardwareVendor -ErrorAction SilentlyContinue).sSCCHardwareVendor
-            Model = $(Get-ADComputer $_.name -Properties sSCCHardwareModel -ErrorAction SilentlyContinue).sSCCHardwareModel
-            Serial = $(Get-ADComputer $_.name -Properties serialnumber -ErrorAction SilentlyContinue).serialnumber[0]
-            PrimaryUser = $_.PrimaryUser
-            CurrentUser = $_.currentlogonuser
-            LastUser = $_.lastlogonuser
-            FirstName = $(Get-ADUser $_.lastlogonuser -ErrorAction SilentlyContinue).givenname 
-            LastName = $(Get-ADUSer $_.lastlogonuser -ErrorAction SilentlyContinue).surname
-            Email = $(Get-ADUser $_.lastlogonuser -Properties emailaddress -ErrorAction SilentlyContinue).emailaddress
+    <# ComputerNames List #>
+    # generate a report from a list of computer names rather than an existing colleciton in MECM. 
+    if ($null -ne $ComputerNames) {
+        $ComputerNames | ForEach-Object{
+            Get-CMDevice -Name $_ | ForEach-Object{
+                [PSCustomObject]$Object = [PSCustomObject]@{
+                    Name = $_.name
+                    Vendor = $(Get-ADComputer $_.name -Properties sSCCHardwareVendor -ErrorAction SilentlyContinue).sSCCHardwareVendor
+                    Model = $(Get-ADComputer $_.name -Properties sSCCHardwareModel -ErrorAction SilentlyContinue).sSCCHardwareModel
+                    Serial = $(Get-ADComputer $_.name -Properties serialnumber -ErrorAction SilentlyContinue).serialnumber[0]
+                    PrimaryUser = $_.PrimaryUser
+                    CurrentUser = $_.currentlogonuser
+                    LastUser = $_.lastlogonuser
+                    FirstName = $(Get-ADUser $_.lastlogonuser -ErrorAction SilentlyContinue).givenname 
+                    LastName = $(Get-ADUSer $_.lastlogonuser -ErrorAction SilentlyContinue).surname
+                    Email = $(Get-ADUser $_.lastlogonuser -Properties emailaddress -ErrorAction SilentlyContinue).emailaddress
+                }
+                $ObjectArray += $Object;
+            }
         }
-        $ObjectArray += $Object;
-    }
+        
+    }else{
+        <# MECM Collection #>
+            # Try to get the CMCollection by name, error handle if not found
+        try {
+            $script:collection = (Get-CMCollection -Name $CollectionName -CollectionType Device)
+        }
+        catch {
+            Write-Error -Message $("Unable to resolve CM Collection {0}" -f $CollectionName) -ErrorAction Stop -Category ObjectNotFound
+        }
 
+        # Make sure that $collection is populated first
+        if($null -ne $collection){
+            # Get the devices in the collection, then mine data to create report
+            Get-CMDevice -Collection $collection | ForEach-Object {
+                [PSCustomObject]$Object = [PSCustomObject]@{
+                    Name = $_.name
+                    Vendor = $(Get-ADComputer $_.name -Properties sSCCHardwareVendor -ErrorAction SilentlyContinue).sSCCHardwareVendor
+                    Model = $(Get-ADComputer $_.name -Properties sSCCHardwareModel -ErrorAction SilentlyContinue).sSCCHardwareModel
+                    Serial = $(Get-ADComputer $_.name -Properties serialnumber -ErrorAction SilentlyContinue).serialnumber[0]
+                    PrimaryUser = $_.PrimaryUser
+                    CurrentUser = $_.currentlogonuser
+                    LastUser = $_.lastlogonuser
+                    FirstName = $(Get-ADUser $_.lastlogonuser -ErrorAction SilentlyContinue).givenname 
+                    LastName = $(Get-ADUSer $_.lastlogonuser -ErrorAction SilentlyContinue).surname
+                    Email = $(Get-ADUser $_.lastlogonuser -Properties emailaddress -ErrorAction SilentlyContinue).emailaddress
+                }
+                $ObjectArray += $Object;
+            }
+        }else{
+            Write-Error -Message "Unable to proceed. Null value passed to -Collection parameter." -Category InvalidArgument -ErrorAction Stop
+        }
+    }
+    
     # Export the results of the report to a CSV file and also display to the console
     $ObjectArray | Export-Csv -NoTypeInformation -Path "$Path\MECM_Report-$(Get-Date -Format FileDate).csv"
     $ObjectArray | Format-Table -AutoSize
