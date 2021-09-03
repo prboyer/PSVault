5049a3b61aac900438b24923cdd9f4b582d500de
Paul Boyer <pboyer2@wisc.edu>
2021-09-03 11:22:56 -0500

    Merging updates from local GroupPolicy repo.

--- GroupPolicy/Check-GPPermissions.ps1
+++ GroupPolicy/Check-GPPermissions.ps1
@@ -1,15 +1,15 @@
-﻿function Check-GPPermissions {
+function Check-GPPermissions {
     <#
     .SYNOPSIS
     Script to report GPOs in a domain that do not have accessible ACLs applied.
 
     .DESCRIPTION
     The script will evaluate which GPOs are lacking 'Apply' permissions for either Authenticated Users or Domain Computers. If neither permission is applied, the group
-    policies themselves will not be applied. However, sometimes when a GPO is to be limited to a specific security group it is necessary to make the ACLs more targeted. With this in mind,
+    policies themselves will not be applied. However, sometimes when a GPO is to be limited to a specific security group it is necessary to make the ACLs more targeted. With this in mind, 
     certain GUIDs can excluded from the report.
 
     .PARAMETER FilePath
-    Path to where the report should be saved. This should be a .txt file.
+    Path to where the report should be saved. This should be a .txt file. 
 
     .PARAMETER Fix
     Switch parameter that will send the script into an interactive mode to fix the missing ACLs on GPOs
@@ -46,11 +46,11 @@
 
     #Get all GPOs in current domain
     [Object[]]$DomainGPOs = Get-GPO -All
-
+    
     # For each GPO in $DomainGPOs, this block of logic checks to see if the GPO has an Apply ACL for either Authenticated Users or Domain Computers
     foreach ($gpo in $DomainGPOs) {
-        if ((($gpo.GetSecurityInfo() | Where-Object { $_.Trustee.Name -eq "Authenticated Users" } | Select-Object Permission).Permission -ine "GpoApply") -and
-            (($gpo.GetSecurityInfo() | Where-Object { $_.Trustee.Name -eq "Domain Computers" } | Select-Object Permission).Permission -ine "GpoApply")) {
+        if ((($gpo.GetSecurityInfo() | ? { $_.Trustee.Name -eq "Authenticated Users" } | Select-Object Permission).Permission -ine "GpoApply") -and 
+            (($gpo.GetSecurityInfo() | ? { $_.Trustee.Name -eq "Domain Computers" } | Select-Object Permission).Permission -ine "GpoApply")) {
             # Add the offending GPOs to the array list. Using Out-Null to suppress the index # returned to the console after adding to the list
             $BadPermissions.Add($gpo.Id.Guid) | Out-Null
         }
@@ -83,14 +83,14 @@
             Write-Host $(Get-GPO -Guid $guid).DisplayName -ForegroundColor Yellow
             (Get-GPO -Guid $guid).GetSecurityInfo() | Format-Table
             [int]$z = Read-Host -Prompt "Which ACL would you like to apply? `n[1] Authenticated Users `n[2] Domain Computers `n[3] Both`n[0] Exit`nPress `'Enter`' to skip without making changes"
-
+            
             # add the missing ACL specified by the user
             switch ($z) {
                 1 { Get-GPO -Guid $guid | Set-GPPermission -PermissionLevel GpoApply -TargetName "Authenticated Users" -TargetType Group }
                 2 { Get-GPO -Guid $guid | Set-GPPermission -PermissionLevel GpoApply -TargetName "Domain Computers" -TargetType Group }
                 3 {
                     Get-GPO -Guid $guid | Set-GPPermission -PermissionLevel GpoApply -TargetName "Authenticated Users" -TargetType Group;
-                    Get-GPO -Guid $guid | Set-GPPermission -PermissionLevel GpoApply -TargetName "Domain Computers" -TargetType Group;
+                    Get-GPO -Guid $guid | Set-GPPermission -PermissionLevel GpoApply -TargetName "Domain Computers" -TargetType Group; 
                 }
                 0 { exit; }
                 Default { break; }
--- GroupPolicy/Run-GPOBackup.ps1
+++ GroupPolicy/Run-GPOBackup.ps1
@@ -1,23 +1,23 @@
-﻿function Run-GPOBackup {
+function Run-GPOBackup {
     <#
     .SYNOPSIS
     All-in-one GPO Backup Script. It leverages external modules/functions to create a robust backup of Group Policies in a domain.
-
+    
     .DESCRIPTION
     The script runs BackUp_GPOs.ps1 and Get-GPLinks.ps1 externally to generate additional backup content. The script will backup all GPOs in the domain, as well as HTML
-    reports for each GPO indicating what they do. Further, a CSV report is included. The GPO linkage to OUs is also included in both CSV and TXT reports.
+    reports for each GPO indicating what they do. Further, a CSV report is included. The GPO linkage to OUs is also included in both CSV and TXT reports. 
     The script also grabs a copy of the domain SYSVOL unless the -SkipSysvol parameter is supplied.
     The idea is that this backup is all-encompassing and would constitue a disaster recovery restore.
-
+    
     .PARAMETER BackupFolder
     Path to where the backups should bs saved
-
+    
     .PARAMETER Domain
     The domain against which backups are being run. If no value is supplied, the script will implicitly grab the domain from the machine it is running against.
-
+    
     .PARAMETER BackupsToKeep
     Parameter that indicates how many previous backups to keep. Once the backup directory contains X backups, the oldest backups are then removed. By default, 10 backups are kept.
-
+    
     .PARAMETER SkipSysvol
     Parameter that tells the script to forego backing up the domain SYSVOL elements (PolicyDefiniitions, StarterGPOs, and scripts)
 
@@ -26,7 +26,7 @@
 
     .OUTPUTS
     A .zip archive containing all necessary backup information to restore a GPO environment
-
+    
     .NOTES
         Author: Paul Boyer
         Date: 5-5-21
@@ -54,7 +54,7 @@
         $SkipSysvol
     )
     #Requires -Module ActiveDirectory
-
+    
     # Import required module
     Import-Module $PSScriptRoot\External\GPFunctions.psm1
 
@@ -67,7 +67,7 @@
 
         # Variable for today's date
         [String]$global:DATE = Get-Date -Format FileDateTimeUniversal
-
+        
         # Variable for logging the timestamp
         [String]$LOGDATE = Get-Date -Format "G"
 
@@ -79,7 +79,7 @@
         if ($BackupsToKeep -ne $null) {
             $KEEP = $BackupsToKeep
         }
-
+    
     ##
     Write-Information $DATE -InformationVariable +INFO
 
@@ -101,8 +101,8 @@
 
     # Start GPO Backup Job (takes parameters in positional order only)
     Write-Information ("{0}`tBegin local background job: BackupJob - Executes BackUp_GPOS.ps1 `n`t`tBacking up GPOs to {1}" -f $LOGDATE,$Temp) -InformationVariable +INFO -InformationAction Continue
-    $BackupJob = Start-Job -Name "BackupJob" -FilePath $global:BACKUP_GPOS -ArgumentList $BackupDomain,$Temp
-
+    $BackupJob = Start-Job -Name "BackupJob" -FilePath $global:BACKUP_GPOS -ArgumentList $BackupDomain,$Temp 
+  
     # Start GPO Links Job
     Write-Information ("{0}`tBegin local background job: LinksJob - Executes Get-GPLinks.ps1 `n`t`tBacking up Links to {1}" -f $LOGDATE,$Temp) -InformationVariable +INFO -InformationAction Continue
     $LinksJob = Start-Job -Name "LinksJob" -ArgumentList $Temp -ScriptBlock {
@@ -112,7 +112,7 @@
         # Run the script
         Get-GPLinks -BothReport -Path "$args"
     }
-
+    
     <# SysVol Backup #>
         # Only perform the Sysvol backup if the -SkipSysvol parameter is not supplied
         if(-not $SkipSysvol){
@@ -128,7 +128,7 @@
                 $SysvolJob = Start-Job -Name "SysvolJob" -ArgumentList $Sysvol,$Temp -ScriptBlock {
                     try{
                         # Copy the contents from Sysvol (keeping the directory structure the same) to the backup folder
-                        Copy-Item -Path "$($args[0])\Policies\PolicyDefinitions" -Recurse -Destination "$($args[1])\Sysvol\$((Get-ADDomain | Select-Object Forest).Forest)\Policies"
+                        Copy-Item -Path "$($args[0])\Policies\PolicyDefinitions" -Recurse -Destination "$($args[1])\Sysvol\$((Get-ADDomain | Select-Object Forest).Forest)\Policies\PolicyDefinitions\"
                         Copy-Item -Path "$($args[0])\scripts" -Recurse -Destination "$($args[1])\Sysvol\$((Get-ADDomain | Select-Object Forest).Forest)\scripts\"
                         Copy-Item -Path "$($args[0])\StarterGPOs" -Recurse -Destination "$($args[1])\Sysvol\$((Get-ADDomain | Select-Object Forest).Forest)\StarterGPOs\"
                     }
@@ -159,7 +159,7 @@
     # Perform cleanup of older backups if the directory has more than 10 archives
     Write-Information ("{0}`tPerform cleanup of older backups if the directory has more than $KEEP archives" -f $LOGDATE) -InformationVariable +INFO
     if ((Get-ChildItem $backupFolder -Filter "*.zip"| Measure-Object).Count -gt $KEEP+1) {
-
+   
         # Delete backups older than the specified retention period, however keep a minimum of 5 recent backups.
         Get-ChildItem $backupFolder -Filter "*.zip" | Sort-Object -Property LastWriteTime -Descending | Select-Object -Skip $KEEP | Remove-Item -Recurse -Force
     }
