4196c8efa5ede0a00a9e8e100bc5412758c59a79
Paul Boyer <pboyer2@wisc.edu>
2022-02-08 15:14:41 -0600

    Updated Send-Email
    Updated the Send-Email function to add in additional parameters. Now
    values can be specified at runtime on the command line rather than
    hard-coding them.

--- Utilities/Send-Email.ps1
+++ Utilities/Send-Email.ps1
@@ -1,41 +1,72 @@
 function Send-Email {
-    <#
+<#
     .SYNOPSIS
     Script that generates and sends an Email message.
-    
+
     .DESCRIPTION
     Due to the deprecation of Send-MailMessage cmdlet, this script leverages the .NET libraries to construct and email and send it to a user.
-    
+
     .PARAMETER To
-    String array of email addresses 
-    
+    String array of email addresses
+
     .PARAMETER CC
-    String array of email addresses 
-    
+    String array of email addresses
+
     .PARAMETER BCC
-    String array of email addresses 
-    
+    String array of email addresses
+
     .PARAMETER Subject
     String for the subject line of the mail message
 
     .PARAMETER Attachments
     String array of paths to files to attach to the email
-    
+
     .PARAMETER Body
     String to place into the body of the email
-    
-    .PARAMETER Unauthenticated
-    Switch that indicates the SMTP server does not require credentials
-    
+
+    .PARAMETER Authenticated
+    Switch that indicates the SMTP server requires credentials.
+
+    .PARAMETER Username
+    String for the username to use when authenticating to the SMTP server.
+
+    .PARAMETER Password
+    String for the password to use when authenticating to the SMTP server. This is parameter is expecting a SecureString.
+
     .PARAMETER HTML
     Switch that indicates that the -Body String is formatted in HTML
 
+    .PARAMETER SMTPServer
+    URI of the SMTP server
+
+    .PARAMETER SMTPPort
+    Port of the SMTP server. If no port is specified, the default port is 25.
+
+    .PARAMETER EnableSSL
+    Switch parameter to tell the server to use SSL. By default, SSL is not used.
+
+    .PARAMETER FromAddress
+    String for the From address of the email.
+
+    .PARAMETER FromName
+    String for the From name of the email.
+
+    .PARAMETER ReplyToAddress
+    String for the ReplyTo address of the email.
+
     .EXAMPLE
-    Send-Email -To bbadger@wisc.edu -Subject "Tuition is Due" -Body "Your Tuition Bill is available online"
-    
+    Send-Email -To bbadger@wisc.edu -Subject "Tuition is Due" -Body "Your Tuition Bill is available online" -FromAddress "noreply@wisc.edu" -FromName "WISC Tuition" -ReplyToAddress "support@wisc.edu" -SMTPServer "smtp.gmail.com"
+
+    Sends an unauthenticated and non-SSL email to the specified recipient.
+
+    .EXAMPLE
+    Send-Email -To bbadger@wisc.edu -Subject "Tuition is Due" -Body "Your Tuition Bill is available online" -FromAddress "noreply@wisc.edu" -FromName "WISC Tuition" -ReplyToAddress "support@wisc.edu" -SMTPServer "smtp.gmail.com" -SMTPPort 587 -EnableSSL -Authenticated -Username "username" -Password "password"
+
+    Sends an authenticated email with SSL to the specified recipient. The SMTP server is gmail.com and the port is 587.
+
     .NOTES
         Author: Paul Boyer
-        Date: 4-9-20201
+        Date: 4-9-2021
 
     .LINK
     https://stackoverflow.com/questions/36355271/how-to-send-email-with-powershell
@@ -69,25 +100,63 @@ function Send-Email {
         [ValidateNotNullOrEmpty()]
         [String]
         $Body,
+        [Parameter(ParameterSetName="Authenticated", Mandatory=$false)]
+        [switch]
+        $Authenticated,
+        [Parameter(ParameterSetName="Authenticated", Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Username,
+        [Parameter(ParameterSetName="Authenticated", Mandatory=$true)]
+        [SecureString]
+        [ValidateNotNullOrEmpty()]
+        $Password,
         [Parameter()]
         [switch]
-        $Unauthenticated,
+        $HTML,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $SMTPServer,
+        [Parameter()]
+        [int]
+        $SMTPPort,
         [Parameter()]
         [switch]
-        $HTML
+        $EnableSSL,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $FromAddress,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $FromName,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $ReplyToAddress
 
     )
     <# Variables #>
         # SMTP Server Configuration
-        [String]$SMTP_SERVER = ""
-        [Int]$SMTP_PORT = 587
-        [String]$USERNAME = ""
-        [String]$PASSWORD = ""
+        [String]$SMTP_SERVER = $SMTPServer
+
+        if ($SMTPPort -eq $null) {
+            [int]$SMTP_PORT = 25
+        } else {
+            [int]$SMTP_PORT = $SMTPPort
+        }
+
+        if ($Authenticated) {
+            [String]$USERNAME = $Username
+            [String]$PASSWORD = $Password
+        }
 
         # Sender Information
-        [String]$SENDER_NAME = ""
-        [String]$SENDER_EMAIL = ""
-        [String]$SENDER_REPLYTO = ""
+        [String]$SENDER_NAME = $FromName
+        [String]$SENDER_EMAIL = $FromAddress
+        [String]$SENDER_REPLYTO = $ReplyToAddress
 
     # Create new MailMessage Object
     [System.Net.Mail.MailMessage]$Message = [System.Net.Mail.MailMessage]::new();
@@ -110,7 +179,7 @@ function Send-Email {
                 $Message.CC.Add($_)
             }
         }
-    
+
         # Add the 'BCC' addresses
         if($null -ne $BCC){
             $BCC | ForEach-Object {
@@ -137,7 +206,14 @@ function Send-Email {
 
     # Send the message
     [Net.Mail.SmtpClient]$Smtp = [Net.Mail.SmtpClient]::new()
-    $Smtp.EnableSsl = $true;
+
+    # If the -EnableSSL parameter is passed, set the SMTP client to use SSL
+    if ($EnableSSL) {
+        $Smtp.EnableSsl = $true;
+    }else{
+        $Smtp.EnableSsl = $false;
+    }
+
     $Smtp.Port = $SMTP_PORT
     $Smtp.Host = $SMTP_SERVER
 
@@ -148,7 +224,7 @@ function Send-Email {
         $Credentials.Password = $PASSWORD
         $Smtp.Credentials = $Credentials;
     }
-    
+
     $Smtp.Send($Message);
 
     # Cleanup
